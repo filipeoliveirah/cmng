ead1e37d7dfe707be8fb79c3031c9833baf7dbd1
Filipe Oliveira <filipeoliveirah@gmail.com>
2018-05-02 15:00:52 -0300

    Homologacao

--- wp-content/themes/dna/incs/slider.php
+++ wp-content/themes/dna/incs/slider.php
@@ -1,10 +1,22 @@
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators-->
+  <? $wp_query = new WP_Query(); ?>
   <div class="visible-lg">
     <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <?
+        $inc_slide = 0;
+        $args = array( 
+          'category_name' => 'destaque',
+          //'posts_per_page'=> 3
+        );
+        $wp_query->query($args);
+        while ($wp_query->have_posts()) : $wp_query->the_post();
+      ?>  
+      <li data-target="#carousel-example-generic" data-slide-to="<? echo $inc_slide; ?>" class="<? if($inc_slide == 0): echo 'active';  endif;?>"></li>
+      <?
+        $inc_slide++;
+        endwhile;
+      ?>
     </ol>
   </div>
 
@@ -12,16 +24,16 @@
   
   <div class="carousel-inner" role="listbox">
     <? 
-      $inc = 1;
+      $inc = 0;
       $wp_query = new WP_Query();
       $args = array( 
         'category_name' => 'destaque',
-        'posts_per_page'=> 3
+        //'posts_per_page'=> 3
       );
       $wp_query->query($args);
       while ($wp_query->have_posts()) : $wp_query->the_post();
     ?>
-    <div class="item <? if($inc == 1): echo 'active'; endif; ?>">
+    <div class="item <? if($inc == 0): echo 'active'; endif; ?>">
       <?php the_post_thumbnail('full'); ?>
       <div class="carousel-caption">
       <h2><?php the_title(); ?></h2>
